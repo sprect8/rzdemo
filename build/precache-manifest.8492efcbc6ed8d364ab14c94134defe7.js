62baa79177ec4bd681fda74f9295fad0694027c7
vphu <vphu@clarity.my>
2018-10-27 09:51:32 +0800

    Adding build

---
+++ build/precache-manifest.8492efcbc6ed8d364ab14c94134defe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c62176582468bafbe3e1",
+    "url": "/static/js/main.c6217658.chunk.js"
+  },
+  {
+    "revision": "11fc1441db8620e9ef8a",
+    "url": "/static/js/1.11fc1441.chunk.js"
+  },
+  {
+    "revision": "c62176582468bafbe3e1",
+    "url": "/static/css/main.3bb6b43c.chunk.css"
+  },
+  {
+    "revision": "a01281207c9309b2ffe3ecd7e94118b5",
+    "url": "/index.html"
+  }
+];
